50e36a15009119901182cfc813b98acaf6d86620
Kent Nagata <ngtknt@me.com>
2018-05-23 18:06:25 +0900

    Fix

--- Formula/xcode-template.rb
+++ Formula/xcode-template.rb
@@ -1,11 +1,11 @@
 class XcodeTemplate < Formula
-  VERSION = "1.0.0"
+  VERSION = "0.1.0"
 
   desc "👩‍💻xcode-template makes it easy to create a template and share with your project"
   homepage "https://github.com/ngtk/xcode-template"
   version VERSION
   url "https://github.com/ngtk/xcode-template/releases/download/v#{VERSION}/xcode-template_#{VERSION}_darwin_386.zip"
-  sha256 "3c21c396b64289aa58a6c00b39ee279e282680cde7df31e5e530a7fe7e4bc32b"
+  sha256 "c2cfbf4ef9445c1b36dbbbc8192fc72e275fe0421bf967c7f60cbdd6a00550af"
 
   def install
     bin.install "xcode-template"
